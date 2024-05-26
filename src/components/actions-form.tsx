b7ce25135ec2db1f0b3d9a6c1d8e4b6b27a0c032
Sahil Khadka <sahilkhadka767@gmail.com>
2024-05-26 18:32:35 +0545

    button added

---
+++ src/components/actions-form.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { unfollowUser } from "@/lib/actions";
+
+interface Props {
+	followed: boolean;
+	userName: string;
+}
+
+export default function ActionsForm({ followed, userName }: Props) {
+	return (
+		<button
+			onClick={async () => {
+				await unfollowUser(userName);
+			}}
+			type='submit'
+			className={`text-sm w-24 text-center border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group ${
+				followed
+					? "bg-red-950 text-red-500 border-red-500"
+					: "bg-green-950 text-green-400 border-green-400"
+			}`}
+		>
+			<span
+				className={`bg-red-500 shadow-red-500 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.6)] ${
+					followed
+						? "bg-red-500 shadow-red-500"
+						: "bg-green-400 shadow-green-400"
+				}`}
+			></span>
+			{followed ? "Unfollow" : "Follow"}
+		</button>
+	);
+}
